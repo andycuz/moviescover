6a2280156cc0cef020e3145c36eecaadbec06b6f
Andrea Cossavella <andrea.cossavella@gmail.com>
2023-10-24 11:06:05 +0200

    - Update searchMovies function

--- moviescover/src/components/searchMovies.jsx
+++ moviescover/src/components/searchMovies.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchMovies() {
+  const [query, setQuery] = useState("");
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIES_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
       </label>
@@ -11,6 +26,7 @@ export default function SearchMovies() {
         type="text"
         name="query"
         placeholder="ex. Old Boy"
+        onChange={(e) => setQuery(e.target.value)}
       />
       <button className="button" type="submit">
         Search
