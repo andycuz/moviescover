f77f2e2f969171a76ac291e906ad45f6e9d155f0
Andrea Cossavella <andrea.cossavella@gmail.com>
2023-10-24 11:43:52 +0200

    - implented basic functionality

--- moviescover/src/components/searchMovies.jsx
+++ moviescover/src/components/searchMovies.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import MovieCard from "./movieCard";
 
 export default function SearchMovies() {
   const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (e) => {
     e.preventDefault();
@@ -11,26 +13,35 @@ export default function SearchMovies() {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+      setMovies(data.results);
     } catch (err) {
       console.error(err);
     }
   };
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        placeholder="ex. Old Boy"
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="ex. Old Boy"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
   );
 }
